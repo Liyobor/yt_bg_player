345b030f24c53dbd039b355bae564b6f8e1ce11f
Ray_Shangguan <a0963382999@gmail.com>
2023-10-24 14:25:56 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/yt_bg_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yt_bg_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'logic.dart';
+
+class HomePage extends StatelessWidget {
+  final logic = Get.put(HomeLogic());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
